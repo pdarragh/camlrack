f422a2108adefad57480d13912585a63da776f0d
Pierce Darragh <pdarragh@wireless-10-104-245-185.umd.edu>
2021-11-22 15:05:02 -0500

    tokenize/tokenTypes: implement string_of_token

--- src/tokenize/tokenTypes.ml
+++ src/tokenize/tokenTypes.ml
@@ -3,3 +3,10 @@ type token =
   | RParen
   | Integer of int
   | Symbol of string
+
+let string_of_token (t : token) : string =
+  match t with
+  | LParen -> "("
+  | RParen -> ")"
+  | Integer i -> string_of_int i
+  | Symbol s -> s
