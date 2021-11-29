5305430356168ffa86114f4f1b2214c77ecff89e
Pierce Darragh <pdarragh@Pierces-MacBook-Pro.local>
2021-11-29 13:03:47 -0700

    test/*: begin implementing parsing tests

---
+++ test/testParse.ml
@@ -0,0 +1,26 @@
+open OUnit2
+
+open Tokenize
+open Parse
+open Parse.Sexp
+
+let test_parse (input, sexp : string * sexp) _ =
+  assert_equal sexp (parse (tokenize input))
+
+let parse_tests =
+  [ ("123", Sexp [Integer 123])
+  ; ("foo", Sexp [Symbol "foo"])
+  ; ("(foo (bar baz) quux)",
+    (Sexp [Sexp [Symbol "foo"; Sexp [Symbol "bar"; Symbol "baz"]; Symbol "quux"]]))
+  ]
+
+let tests =
+  List.mapi
+    (fun i p -> ("test:" ^ string_of_int i ^ ":<\"" ^ fst p ^ "\">") >:: (test_parse p))
+    parse_tests
+
+let suite =
+  "parse" >:::
+  List.concat [tests]
+
+let () = run_test_tt_main suite
