38920d327c548f1a237e98d011435322a932b5e4
Pierce Darragh <pdarragh@Pierces-MacBook-Pro.local>
2022-01-06 23:20:35 -0700

    camlrack/src/camlrack: add pattern conversion functions

--- camlrack/src/camlrack.ml
+++ camlrack/src/camlrack.ml
@@ -9,4 +9,12 @@ let sexp_of_string (s : string) : sexp option = Parse.parse s
 
 let sexp_of_string_exn (s : string) : sexp = Parse.parse_exn s
 
+let sexp_pattern_of_string (s : string) : sexp_pattern option =
+  match sexp_of_string s with
+  | Some se -> Some (sexp_pattern_of_sexp se)
+  | None -> None
+
+let sexp_pattern_of_string_exn (s : string) : sexp_pattern =
+  sexp_pattern_of_sexp (sexp_of_string_exn s)
+
 let sexp_match (p : sexp_pattern) (se : sexp) : bool = Match.sexp_match p se
