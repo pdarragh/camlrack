2f83cdb0b23ea83df8fdd59bcf1ffb224dbcb5d1
Pierce Darragh <pdarragh@Pierces-MacBook-Pro.local>
2022-01-13 16:52:30 -0700

    ppx_camlrack/test/*: revise tests for ppx_spat

--- ppx_camlrack/test/test_spat.expected.ml
+++ ppx_camlrack/test/test_spat.expected.ml
@@ -6,21 +6,63 @@ let test_match (_ : sexp) (_ : sexp_pattern) =
       Camlrack.SExp [Camlrack.Symbol "x"; Camlrack.Symbol "y"];
       Camlrack.SExp
         [Camlrack.Symbol "+"; Camlrack.Symbol "x"; Camlrack.Symbol "y"]] in
-  if
-    Camlrack.Match.sexp_match
-      (Camlrack.Match.SPat
-         [Camlrack.Match.PSymbol "lambda";
-         Camlrack.Match.SPat
-           [Camlrack.Match.SYMBOL; Camlrack.Match.PSymbol "..."];
-         Camlrack.Match.ANY;
-         Camlrack.Match.PSymbol "..."]) sexp
-  then sexp
-  else
-    if
-      Camlrack.Match.sexp_match
-        (Camlrack.Match.SPat
-           [Camlrack.Match.PSymbol "+";
-           Camlrack.Match.INTEGER;
-           Camlrack.Match.INTEGER]) sexp
-    then sexp
-    else sexp
+  match sexp with
+  | Camlrack.SExp [] -> "INTEGER LIST"
+  | Camlrack.SExp ((Camlrack.Integer _)::[]) -> "INTEGER LIST"
+  | Camlrack.SExp ((Camlrack.Integer _)::(Camlrack.Integer _)::[]) ->
+      "INTEGER LIST"
+  | Camlrack.SExp ((Camlrack.SExp subgroup1)::[]) when
+      List.for_all (Camlrack.sexp_match Camlrack.INTEGER) subgroup1 ->
+      "INTEGER LIST"
+  | Camlrack.SExp ((Camlrack.Symbol "lambda")::(Camlrack.SExp [])::[]) ->
+      "LAMBDA EXPRESSION"
+  | Camlrack.SExp ((Camlrack.Symbol "lambda")::(Camlrack.SExp
+      ((Camlrack.Symbol _)::[]))::[]) -> "LAMBDA EXPRESSION"
+  | Camlrack.SExp ((Camlrack.Symbol "lambda")::(Camlrack.SExp
+      ((Camlrack.Symbol _)::(Camlrack.Symbol _)::[]))::[]) ->
+      "LAMBDA EXPRESSION"
+  | Camlrack.SExp ((Camlrack.Symbol "lambda")::(Camlrack.SExp ((Camlrack.SExp
+      subgroup1)::[]))::[]) when
+      List.for_all (Camlrack.sexp_match Camlrack.SYMBOL) subgroup1 ->
+      "LAMBDA EXPRESSION"
+  | Camlrack.SExp ((Camlrack.Symbol "lambda")::(Camlrack.SExp [])::_::[]) ->
+      "LAMBDA EXPRESSION"
+  | Camlrack.SExp ((Camlrack.Symbol "lambda")::(Camlrack.SExp
+      ((Camlrack.Symbol _)::[]))::_::[]) -> "LAMBDA EXPRESSION"
+  | Camlrack.SExp ((Camlrack.Symbol "lambda")::(Camlrack.SExp
+      ((Camlrack.Symbol _)::(Camlrack.Symbol _)::[]))::_::[]) ->
+      "LAMBDA EXPRESSION"
+  | Camlrack.SExp ((Camlrack.Symbol "lambda")::(Camlrack.SExp ((Camlrack.SExp
+      subgroup1)::[]))::_::[]) when
+      List.for_all (Camlrack.sexp_match Camlrack.SYMBOL) subgroup1 ->
+      "LAMBDA EXPRESSION"
+  | Camlrack.SExp ((Camlrack.Symbol "lambda")::(Camlrack.SExp [])::_::_::[])
+      -> "LAMBDA EXPRESSION"
+  | Camlrack.SExp ((Camlrack.Symbol "lambda")::(Camlrack.SExp
+      ((Camlrack.Symbol _)::[]))::_::_::[]) -> "LAMBDA EXPRESSION"
+  | Camlrack.SExp ((Camlrack.Symbol "lambda")::(Camlrack.SExp
+      ((Camlrack.Symbol _)::(Camlrack.Symbol _)::[]))::_::_::[]) ->
+      "LAMBDA EXPRESSION"
+  | Camlrack.SExp ((Camlrack.Symbol "lambda")::(Camlrack.SExp ((Camlrack.SExp
+      subgroup1)::[]))::_::_::[]) when
+      List.for_all (Camlrack.sexp_match Camlrack.SYMBOL) subgroup1 ->
+      "LAMBDA EXPRESSION"
+  | Camlrack.SExp ((Camlrack.Symbol "lambda")::(Camlrack.SExp
+      [])::(Camlrack.SExp subgroup2)::[]) when
+      List.for_all (Camlrack.sexp_match Camlrack.ANY) subgroup2 ->
+      "LAMBDA EXPRESSION"
+  | Camlrack.SExp ((Camlrack.Symbol "lambda")::(Camlrack.SExp
+      ((Camlrack.Symbol _)::[]))::(Camlrack.SExp subgroup2)::[]) when
+      List.for_all (Camlrack.sexp_match Camlrack.ANY) subgroup2 ->
+      "LAMBDA EXPRESSION"
+  | Camlrack.SExp ((Camlrack.Symbol "lambda")::(Camlrack.SExp
+      ((Camlrack.Symbol _)::(Camlrack.Symbol _)::[]))::(Camlrack.SExp
+      subgroup2)::[]) when
+      List.for_all (Camlrack.sexp_match Camlrack.ANY) subgroup2 ->
+      "LAMBDA EXPRESSION"
+  | Camlrack.SExp ((Camlrack.Symbol "lambda")::(Camlrack.SExp ((Camlrack.SExp
+      subgroup1)::[]))::(Camlrack.SExp subgroup2)::[]) when
+      (List.for_all (Camlrack.sexp_match Camlrack.ANY) subgroup2) &&
+        (List.for_all (Camlrack.sexp_match Camlrack.SYMBOL) subgroup1)
+      -> "LAMBDA EXPRESSION"
+  | _ -> "OTHER"
--- ppx_camlrack/test/test_spat.ml
+++ ppx_camlrack/test/test_spat.ml
@@ -1,6 +1,6 @@
 open Camlrack
 let test_match (_ : sexp) (_ : sexp_pattern) =
   match%spat "(lambda (x y) (+ x y))" with
-  | "{lambda {SYMBOL ...} ANY ...}" -> sexp
-  | "{+ INTEGER INTEGER}" -> sexp
-  | _ -> sexp
+  | "{INTEGER ...}" -> "INTEGER LIST"
+  | "{lambda {SYMBOL ...} ANY ...}" -> "LAMBDA EXPRESSION"
+  | _ -> "OTHER"
