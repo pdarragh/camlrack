296b4f73742119de9c1e0777754c1376a4bbaf06
Pierce Darragh <pierce.darragh@gmail.com>
2022-01-25 23:29:12 -0700

    ppx_camlrack/src/ppx_spat: refactor to improve legibility

--- ppx_camlrack/src/ppx_spat.ml
+++ ppx_camlrack/src/ppx_spat.ml
@@ -12,8 +12,6 @@ let replicate (n : int) (x : 'a) : 'a list =
   in
   replicate' n []
 
-let subgroup_repeat_count = 2
-
 let scrutinee_match_name = "sexp"
 
 type conditional_pattern = pattern * (expression list)
@@ -34,9 +32,67 @@ let rec sexp_pattern_exp_of_sexp_pattern ~loc (spat : sexp_pattern) : expression
   | PSymbol  s    -> [%expr Camlrack.PSymbol [%e estring ~loc s]]
   | SPat     pats -> [%expr Camlrack.SPat [%e elist ~loc (List.map (sexp_pattern_exp_of_sexp_pattern ~loc) pats)]]
 
+(* Produces a list of patterns corresponding to 0-`len` repetitions of the given
+   pattern. *)
+let rec replicate_pattern
+    (pat : pattern)
+    (pats : pattern list list)
+    (len : int) : pattern list list =
+  if len = 0
+  then
+    (* Add the empty list for zero matches. *)
+    [] :: pats
+  else
+    (* Add the pattern repeated `len` times. *)
+    let sexp_pat = (replicate len pat) in
+    replicate_pattern pat (sexp_pat :: pats) (len - 1)
+
+(* The number of times to expand a subgroup before using a variable match. *)
+let subgroup_repeat_count = 2
+
+(* Given a pattern that is meant to repeat (i.e., via a "..." symbol), produces
+   the needed repetitions of that pattern. *)
+let build_replicated_patterns
+    ~loc
+    (fresh_groupname : (unit -> label))
+    (sexp_pat : sexp_pattern)
+    (pat : pattern)
+    (conds : expression list)
+    (add_var_cpat : bool) : (pattern list * expression list) list =
+  let open Ast_builder.Default in
+  let replicated_patterns = replicate_pattern pat [] subgroup_repeat_count in
+  let fixed_cpats = List.map (fun p -> (p, conds)) replicated_patterns in
+  let groupname = fresh_groupname () in (* FIXME: move into then-body *)
+  if add_var_cpat
+  then
+    let var_cpat = ([[%pat? Camlrack.SExp [%p pvar ~loc groupname]]],
+                    [%expr (List.for_all
+                              (Camlrack.sexp_match
+                                 [%e sexp_pattern_exp_of_sexp_pattern ~loc sexp_pat])
+                              [%e evar ~loc groupname])]
+                    :: conds) in
+    List.append fixed_cpats [var_cpat]
+  else fixed_cpats
+
+(* Constructs a guard for the revised match case. *)
+let build_guard ~loc (c : case) (conds : expression list) : expression option =
+  let rec and_conds (conds : expression list) : expression =
+    match conds with
+    | [] -> failwith "invalid" (* FIXME *)
+    | cond :: [] -> cond
+    | cond :: conds' -> [%expr [%e cond] && [%e and_conds conds']] in
+  match conds with
+  | [] -> c.pc_guard
+  | _ ->
+    let anded_conds = and_conds conds in
+    (match c.pc_guard with
+     | Some g -> Some [%expr [%e g] && [%e anded_conds]]
+     | None -> Some anded_conds)
+
 let convert_case (c : case) : (case list) option =
   let open Ast_builder.Default in
   let convert_sexp_pattern ~loc (pat : sexp_pattern) : conditional_pattern list =
+    (* Sub-group names are tracked throughout an entire case conversion. *)
     let groupno = ref 0 in
     let freshno () = groupno := !groupno + 1; !groupno in
     let fresh_groupname () =
@@ -60,37 +116,6 @@ let convert_case (c : case) : (case list) option =
     (* Converts a list of patterns corresponding to list-like S-Expressions into
        a list of conditional patterns. *)
     and convert_subpats (pats : sexp_pattern list) (add_var_cpat : bool) : conditional_pattern list =
-      let build_replicated_patterns
-          (sexp_pat : sexp_pattern)
-          (pat : pattern)
-          (conds : expression list) : (pattern list * expression list) list =
-        (* Produces a list of patterns corresponding to 0-`len` repetitions of the
-           given pattern. *)
-        let rec replicate_pattern
-            (pat : pattern)
-            (pats : pattern list list)
-            (len : int) : pattern list list =
-          if len = 0
-          then
-            (* Add the empty list for zero matches. *)
-            [] :: pats
-          else
-            (* Add the pattern repeated `len` times. *)
-            let sexp_pat = (replicate len pat) in
-            replicate_pattern pat (sexp_pat :: pats) (len - 1) in
-        let replicated_patterns = replicate_pattern pat [] subgroup_repeat_count in
-        let fixed_cpats = List.map (fun p -> (p, conds)) replicated_patterns in
-        let groupname = fresh_groupname () in
-        if add_var_cpat
-        then
-          let var_cpat = ([[%pat? Camlrack.SExp [%p pvar ~loc groupname]]],
-                          [%expr (List.for_all
-                                    (Camlrack.sexp_match
-                                       [%e sexp_pattern_exp_of_sexp_pattern ~loc sexp_pat])
-                                    [%e evar ~loc groupname])]
-                          :: conds) in
-          List.append fixed_cpats [var_cpat]
-        else fixed_cpats in
       let rec convert_subpats'
           (prevs : (pattern list * expression list) list)
           (sexp_pats : sexp_pattern list) =
@@ -107,7 +132,7 @@ let convert_case (c : case) : (case list) option =
           let replicated_expansions : (pattern list * expression list) list =
             List.flatten
               (List.map
-                 (fun (p, conds) -> build_replicated_patterns sexp_pat p conds)
+                 (fun (p, conds) -> build_replicated_patterns ~loc fresh_groupname sexp_pat p conds add_var_cpat)
                  pat_expansions) in
           let new_prevs : (pattern list * expression list) list =
             List.flatten
@@ -150,21 +175,8 @@ let convert_case (c : case) : (case list) option =
       then [uncond_pat [%pat? Camlrack.SExp []]]
       else converted_subpats in
     convert_pattern pat true in
-  let build_guard ~loc (conds : expression list) : expression option =
-    let rec and_conds (conds : expression list) : expression =
-      match conds with
-      | [] -> failwith "invalid" (* FIXME *)
-      | cond :: [] -> cond
-      | cond :: conds' -> [%expr [%e cond] && [%e and_conds conds']] in
-    match conds with
-    | [] -> c.pc_guard
-    | _ ->
-      let anded_conds = and_conds conds in
-      (match c.pc_guard with
-       | Some g -> Some [%expr [%e g] && [%e anded_conds]]
-       | None -> Some anded_conds) in
   let convert_cond_pat_to_cases ~loc ((pat, conds) : conditional_pattern) : case =
-    case ~lhs:pat ~guard:(build_guard ~loc conds) ~rhs:c.pc_rhs
+    case ~lhs:pat ~guard:(build_guard ~loc c conds) ~rhs:c.pc_rhs
   in
   match c.pc_lhs.ppat_desc with
   | Ppat_any -> Some [c]
